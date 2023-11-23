4eaa63d0c0c05b887b98a57be02242ba7ae1021d
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2023-11-23 09:37:26 +0100

    added routing after review post

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Review;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
@@ -15,6 +16,7 @@ class ReviewController extends Controller
         return DB::select('select * FROM reviews');
     }
     function insertOne(Request $request) {
+        // Try catch if user is not logged in
         $user_id = auth()->user()->id;
         $game_id = 1;
         $title = $request->get('title');
@@ -22,7 +24,8 @@ class ReviewController extends Controller
         $content = $request->get('content');
         $rating = $request->get('rating');
         DB::insert('insert into reviews (user_id, game_id, title, platform, content, rating) values (?, ?, ?, ?, ?, ?)', [$user_id, $game_id, $title, $platform, $content, $rating]);
-        return $title;
+        $previous = url()->previous();
+        return Redirect::to($previous);
     }
     function fetchFromID($userid) {
         return DB::select('select * from reviews where user_id = :user_id', ['user_id' => $userid]);
