0a91b3ac97946f7edf63215c47d82c6319bccdde
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2023-11-27 16:43:29 +0100

    Search works! Hurray!

--- public/js-lul/browse.js
+++ public/js-lul/browse.js
@@ -1,3 +1,5 @@
+let searchableGames = [];
+
 async function getAllGames(){
   const response = await fetch('/games');
   const games = await response.json();
@@ -43,11 +45,70 @@ async function createGameElements(){
         gameContainer.appendChild(description)
         gameContainer.appendChild(rating)
         allGames.appendChild(gameContainer);
-
     })
     console.log(fetchedGames);
 
 
 }
+
+async function getGames() {
+    const fetchData = await fetch('http://localhost:8000/games');
+    const data = await fetchData.json();
+
+    data.forEach((game) => {
+        searchableGames.push(game);
+    })
+    console.log(searchableGames);
+}
+
+function displaySearchedGames(searchGames) {
+    const allGames = document.getElementById("allgames");
+    allGames.innerHTML = '';
+    searchGames.forEach((game) => {
+        const gameContainer = document.createElement('article');
+        gameContainer.id = 'game';
+
+        gameContainer.addEventListener('click', () => {
+            // Redirect to the game page with the specific ID
+            window.location.href = `/gamepage/${game.name}`;
+        });
+
+        const image = document.createElement('img')
+        image.id = 'img';
+        const name = document.createElement('h3');
+        name.id = 'featured-title';
+        const description = document.createElement('p');
+        description.id = 'featured-snippet';
+        const rating = document.createElement('div');
+        rating.id = 'rating';
+        const span1 = document.createElement('span');
+        const span2 = document.createElement('span');
+        const span3 = document.createElement('span');
+        const span4 = document.createElement('span');
+        const span5 = document.createElement('span');
+
+        image.src = game['image_link'];
+        name.textContent = game.name;
+        description.textContent = game.description;
+        rating.textContent = game.aggregate_rating + "/5";
+
+        gameContainer.className = "game_container"
+
+        gameContainer.appendChild(image);
+        gameContainer.appendChild(name);
+        gameContainer.appendChild(description)
+        gameContainer.appendChild(rating)
+        allGames.appendChild(gameContainer);
+    })
+
+}
+
+const searchbar = document.getElementById('searchbar');
+searchbar.addEventListener('keyup', () => {
+    let foundgames = searchableGames.filter(e => e.name.toUpperCase().includes(searchbar.value.toUpperCase()));
+    displaySearchedGames(foundgames);
+});
+
+getGames();
 
 createGameElements();
--- public/js-lul/script.js
+++ public/js-lul/script.js
@@ -1,4 +1,4 @@
-let searchableGames = [];
+
 
 function toggleMenu() {
     const menuLinks = document.querySelector('.menu-links');
@@ -7,21 +7,4 @@ function toggleMenu() {
     menuLinks.classList.toggle('open');
 }
 
-async function getGames() {
-    const fetchData = await fetch('http://localhost:8000/games');
-    const data = await fetchData.json();
 
-    data.forEach((game) => {
-        searchableGames.push(game);
-    })
-    console.log(searchableGames);
-}
-
-
-const searchbar = document.getElementById('searchbar');
-searchbar.addEventListener('keyup', () => {
-    let foundgames = searchableGames.filter(e => e.name.toUpperCase().includes(searchbar.value.toUpperCase()));
-    console.log(foundgames);
-});
-
-getGames();
